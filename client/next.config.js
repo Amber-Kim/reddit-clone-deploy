4af335450464890547e95a45c113b5fbc4e1dae3
Amber-Kim <amber.kje@gmail.com>
2022-08-23 16:37:55 -0400

    changed

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true,
   images: {
     domains: [
-      "www.gravatar.com", "localhost", "ec2-52-15-209-192.us-east-2.compute.amazonaws.com"
+      "www.gravatar.com", 
+      "localhost", 
+      "ec2-18-116-49-47.us-east-2.compute.amazonaws.com"
     ],
   }
 }
