69dbf543db942d6eed3885199c15a6ed73bff6ec
Amber-Kim <[amber.kje@gmail.com]>
2022-09-14 20:58:37 -0400

    Updated AWS DNS

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com", 
       "localhost", 
-      "ec2-3-15-32-248.us-east-2.compute.amazonaws.com"
+      "ec2-3-140-239-88.us-east-2.compute.amazonaws.com"
     ],
   }
 }
